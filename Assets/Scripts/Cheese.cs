1e129f506b34987a7e828de9b0fc902081d9ed65
gustavo andres Barrios Araya <gustavo.barrios@sansano.usm.cl>
2020-05-30 15:13:39 -0400

    Se agrega escena de ejemplo

---
+++ Assets/Scripts/Cheese.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Mirror
+{
+
+    public class Cheese : NetworkBehaviour
+    {
+        public Rigidbody2D rigidbody2d;
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+
+            // only simulate ball physics on server
+            rigidbody2d.simulated = true;
+        }
+
+        // only call this on server
+        [ServerCallback]
+        void OnCollisionEnter2D(Collision2D col)
+        {
+            // Note: 'col' holds the collision information. If the
+            // Ball collided with a racket, then:
+            //   col.gameObject is the racket
+            //   col.transform.position is the racket's position
+            //   col.collider is the racket's collider
+
+            // did we hit a racket? then we need to calculate the hit factor
+            if (col.transform.GetComponent<Player>())
+            {
+                NetworkServer.Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/NetworkManagerQuesix.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Mirror
+{
+    // Custom NetworkManager that simply assigns the correct racket positions when
+    // spawning players. The built in RoundRobin spawn method wouldn't work after
+    // someone reconnects (both players would be on the same side).
+    [AddComponentMenu("")]
+    public class NetworkManagerQuesix : NetworkManager
+    {
+        public Transform leftRacketSpawn;
+        public Transform rightRacketSpawn;
+        public Transform leftCheeseSpawn;
+        public Transform rightCheeseSpawn;
+        GameObject cheese;
+
+        public override void OnServerAddPlayer(NetworkConnection conn)
+        {
+            // add player at correct spawn position
+            Transform start = numPlayers == 0 ? leftRacketSpawn : rightRacketSpawn;
+            GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
+            NetworkServer.AddPlayerForConnection(conn, player);
+
+            // spawn ball if two players
+            if (numPlayers == 2)
+            {
+                for(int i = 0; i < 2; i++)
+                {
+                    if(i == 0)
+                        cheese = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Queso"), leftCheeseSpawn.position, start.rotation);
+                    else
+                        cheese = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Queso"), rightCheeseSpawn.position, start.rotation);
+                    NetworkServer.Spawn(cheese);
+                }
+            }
+        }
+
+        public override void OnServerDisconnect(NetworkConnection conn)
+        {
+            // destroy ball
+            if (cheese != null)
+                NetworkServer.Destroy(cheese);
+
+            // call base functionality (actually destroys the player)
+            base.OnServerDisconnect(conn);
+        }
+    }
+}
