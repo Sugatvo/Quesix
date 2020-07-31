ec3537ace607730c12792c4719cdb24729e0ccf4
Nicolas Acevedo <roth81ly@gmail.com>
2020-07-31 12:20:55 -0400

    added UI frames prefabs

---
+++ Assets/UIManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+
+    public RectTransform mainMenu;
+
+    void Awake()
+    {
+        //singleton patern
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+
+    }
+
+    private void Start()
+    {
+        //on start transition
+        mainMenu.DOAnchorPos(Vector2.zero, 0.5f);
+    }
+
+    void jumpFrame( GameObject frame, float time)
+    {
+        
+    }
+
+}
